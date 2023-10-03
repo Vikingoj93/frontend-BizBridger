9a632d0410cc6009050c589aac126d0914a983fb
Arturo Pacheco <arturojesus.pc@gmail.com>
2023-10-03 10:42:31 -0400

    cambios en estructura de la carpeta components de dashboard

---
+++ src/app/dashboard/components/DiariComponent.tsx
@@ -0,0 +1,16 @@
+'use client'
+export default function TaskComponent() {
+  return (
+    <div>
+
+    <section>
+      <form action="" method="post">
+        
+      </form>
+    </section>
+    <section>
+
+    </section>
+    </div>
+  );
+}
